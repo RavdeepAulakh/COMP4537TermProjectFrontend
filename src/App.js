4ba019dcf206645ba5dbe52d7ee87557f1441f87
teihyung <teihyung@gmail.com>
2024-03-19 19:43:43 -0700

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Signup from './pages/signup';
 import Login from './pages/login';
 import UserPage from './pages/userPage'
 import Index from './pages/index'
+import Navbar from "./components/navbar";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/userPage" element={<UserPage />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Index/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/userPage" element={<UserPage/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { logout } from '../auth/auth'; // Import the logout function
+import '../styles/navbar.css';
+
+function Navbar() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = sessionStorage.getItem('token');
+        setIsLoggedIn(!!token); // Update the state based on the presence of a token
+    }, []);
+
+    return (
+        <nav className="navbar">
+            <h3>
+                {isLoggedIn ? (
+                    <Link to="/userPage" className="navbar-brand">MyApp</Link>
+                ) : (
+                    <Link to="/" className="navbar-brand">MyApp</Link>
+                )}
+            </h3>
+            <ul className="navbar-nav">
+                {isLoggedIn ? (
+                    <>
+                        <li><Link to="/userPage">UserPage</Link></li>
+                        <li><a href="#" onClick={logout}>Logout</a></li>
+                    </>
+                ) : (
+                    <>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/signup">Signup</Link></li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
